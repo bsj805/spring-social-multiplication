677d9dbff23e75ee525dd5db81684e1b7be09ce5
Byeon sungjin <bsj805@naver.com>
2022-05-21 18:41:58 +0900

    [bsj] first project configuration

---
+++ src/main/java/microservices/book/multiplication/SpringMsaApplication.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/microservices/book/multiplication/SpringMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
